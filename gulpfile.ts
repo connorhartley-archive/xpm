6339539f5bd7705220803ca2bbbef4ef5bc63b44
Connor Hartley <vectrixu@gmail.com>
2016-10-02 11:48:44 +1300

    :arrow_up: Update gulp-typescript to 3.0.2

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
-import gulp = require("gulp");
-import typescript = require("gulp-typescript");
+import gulp = require('gulp');
+import typescript = require('gulp-typescript');
 
-const project = typescript.createProject("tsconfig.json");
+const project = typescript.createProject('tsconfig.json');
 
-gulp.task("compile:typescript", () => {
-  let result = project.src()
-        .pipe(project());
-  return result.js.pipe(gulp.dest("lib"));
+gulp.task('compile:typescript', () => {
+	const result = project.src()
+				.pipe(project());
+	return result.js.pipe(gulp.dest('lib'));
 });
