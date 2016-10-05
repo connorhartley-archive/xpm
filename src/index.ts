f916377da9ab974ff14d46535df7419b3edaf4ce
Connor Hartley <vectrixu@gmail.com>
2016-10-05 15:13:27 +1300

    :art: Clean up utility classes and :bug: fix spaces in multilines

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,29 @@
 
 import blessed = require('blessed');
 
-import argv = require('./utils/commandParser');
+import argv = require('./utils/argumentParser');
 
 let view = null;
 
 export const commands = {
-	install: require('./commands/install')()
+  install: require('./commands/install')()
 };
 
 export function init (): void {
-	const command: argv.ParsedCommand = argv.parse(process.argv.slice(2));
+  const command: argv.ParsedCommand = argv.parse(process.argv.slice(2));
 
-	if (!view) {
-		view = blessed.screen({ smartCSR: true, autoPadding: true });
-	}
+  if (!view) {
+    view = blessed.screen({ smartCSR: true });
+  }
 
-	switch (command.action) {
-		case 'i':
-		case 'install':
-			commands.install(command, view);
-			break;
-		}
+  switch (command.action) {
+    case 'i':
+    case 'install':
+      commands.install(command, view);
+      break;
+    default:
+      return;
+  }
 }
 
 init();
