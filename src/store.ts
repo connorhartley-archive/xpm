56c40f10f0469f0aa78d2d9b088fad51b0eba712
Connor Hartley <vectrixu@gmail.com>
2016-10-05 18:12:05 +1300

    :art: Clean up install and make installs args optional

--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,22 @@
 'use strict';
 
-import path = require('path');
+import path = require('path')
 
-const userHome = require('user-home');
-const xdgBasedir = require('xdg-basedir');
+const userHome = require('user-home')
+const xdgBasedir = require('xdg-basedir')
 
-export const getHomeDirectory: string = userHome;
-export const getResourceDirectory: string = path.join(xdgBasedir.data, 'xpm') || path.join(getHomeDirectory, '.xpm');
-export const getConfigDirectory: string = path.join(xdgBasedir.config, 'xpm') || path.join(getHomeDirectory, '.xpm', 'config');
+export const getHomeDirectory: string = userHome
+export const getResourceDirectory: string = path.join(xdgBasedir.data, 'xpm') || path.join(getHomeDirectory, '.xpm')
+export const getConfigDirectory: string = path.join(xdgBasedir.config, 'xpm') || path.join(getHomeDirectory, '.xpm', 'config')
 
-export const getPlatformName: string = process.env.XANITE_PLATFORM_NAME || 'electron';
-export const getPlatformVersion: string = process.env.XANITE_PLATFORM_VERSION || '1.4.1';
-export const getPlatformUrl: string = process.env.XANITE_PLATFORM_URL || 'https://atom.io/download/atom-shell';
-export const getPlatformArch: string = process.platform === 'darwin' ? 'x64' : (process.platform === 'win32' ? 'ia32' : process.arch);
+export const getPlatformName: string = process.env.XANITE_PLATFORM_NAME || 'electron'
+export const getPlatformVersion: string = process.env.XANITE_PLATFORM_VERSION || '1.4.2'
+export const getPlatformUrl: string = process.env.XANITE_PLATFORM_URL || 'https://atom.io/download/atom-shell'
+export const getPlatformArch: string = process.platform === 'darwin' ? 'x64' : (process.platform === 'win32' ? 'ia32' : process.arch)
 
-export const getPackageDirectory: string = path.join(getResourceDirectory, 'packages');
-export const getPackageManagerDirectory: string = path.join(getResourceDirectory, '.pnpm');
-export const getSharedDirectory: string = path.join(getResourceDirectory, '.shared');
-export const getNodeDirectory: string = path.join(getResourceDirectory, '.node-gyp');
+export const getPackageDirectory: string = path.join(getResourceDirectory, 'packages')
+export const getPackageManagerDirectory: string = path.join(getResourceDirectory, '.pnpm')
+export const getSharedDirectory: string = path.join(getResourceDirectory, '.shared')
+export const getNodeDirectory: string = path.join(getResourceDirectory, '.node-gyp')
+
+export const getPackageManagerEntry: string = require.resolve(path.join(getPackageManagerDirectory, 'lib', 'bin') + 'pnpm')
